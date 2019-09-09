63c8faa73b0f4ba49be962a10c9982a44d3cea20
lizushan <347228307@qq.com>
2019-09-10 01:42:04 +0800

    上传

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+public class HelloWorld{
+	public static void main(String [] arge){
+		//System.out.println("HelloWorld!!!");
+		System.out.println("lizushan");
+		/*
+		System.out.println("HelloWorld!!!");
+		System.out.println("HelloWorld!!!");
+		System.out.println("HelloWorld!!!");
+		*/
+
+	}
+}
